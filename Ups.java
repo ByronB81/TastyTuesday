5505071bf61d147f330d0bdbd8ef145518cd04e2
Byron Blocker <byronblocker@gmail.com>
2017-05-16 16:23:03 -0700

    a little polishing

--- Ups.java
+++ Ups.java
@@ -10,12 +10,15 @@ public class Ups {
     System.out.println("Let's build a cube. Enter the X axis dimension in  inches:");
     String strX = console.readLine();
     int userX = Integer.parseInt(strX);
+    System.out.println();
     System.out.println("Okay thanks, that was "+ userX + ". now how about the Y axis?");
     String strY = console.readLine();
     int userY = Integer.parseInt(strY);
+    System.out.println();
     System.out.println("Right on, that was " + userY + ". now let's get the Z axis?");
     String strZ = console.readLine();
     int userZ = Integer.parseInt(strZ);
+    System.out.println();
     System.out.println("And " + userZ + ". Thanks!");
 
     Parcel userParcel = new Parcel(userX, userY, userZ);
@@ -28,15 +31,16 @@ public class Ups {
       if (userVolume < 30) {
         System.out.println("This size is considered STANDARD. No additional cost.");
       } else {
-        System.out.println("This ize is considered LARGE. $2 additional charge");
+        System.out.println("This size is considered LARGE. $2 additional charge");
         userParcel.price = userParcel.price + 2;
       }
     }
+    System.out.println("subtotal: $" + userParcel.price);
+    System.out.println();
 
     System.out.println("Now, what is the weight of your parcel in pounds?");
     String strWeight = console.readLine();
     int userWeight = Integer.parseInt(strWeight);
-    System.out.println(userWeight);
 
     userParcel.weight = userWeight;
     userParcel.sort =  userParcel.sorted(userWeight);
@@ -47,6 +51,8 @@ public class Ups {
       System.out.println("This item is considered HEAVY. $2 additional charge");
       userParcel.price = userParcel.price + 2;
     }
+    System.out.println("subtotal: $" + userParcel.price);
+    System.out.println();
 
     System.out.println("How far are you shipping the parcel in miles?");
     String strDistance = console.readLine();
@@ -60,6 +66,8 @@ public class Ups {
       System.out.println("Long distance charges apply to deliveries over 1000 miles. Additional charge of $10");
       userParcel.price = userParcel.price + 10;
     }
+    System.out.println("subtotal: $" + userParcel.price);
+    System.out.println();
 
     System.out.println("Lastly, how fast do you want it? Overnight, 2-day, standard");
     String strDelivery = console.readLine();
@@ -74,7 +82,8 @@ public class Ups {
     }
 
 
-    System.out.println("$" + userParcel.price);
+    System.out.println("Total: $" + userParcel.price);
+    System.out.println();
 
 
 
